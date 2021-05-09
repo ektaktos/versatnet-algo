ddc176fd1a8f5c48962fea77b1862a8413496b32
Wale Timothy <awtim01@gmail.com>
2021-05-09 16:56:39 +0100

    Added try catch to redfiles function

--- mainController.js
+++ mainController.js
@@ -5,13 +5,17 @@ const config = require('./config');
 exports.readFiles = async (req, res) => {
   const wireguardPath = `algo/configs/${config.ip_address}/wireguard`;
   const directoryPath = path.join(__dirname, wireguardPath);
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-
-    files.forEach( (file) => {
-      console.log(file);
+  try {
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        return console.log('Unable to scan directory: ' + err);
+      }
+  
+      files.forEach( (file) => {
+        console.log(file);
+      });
     });
-  });
+  } catch (error) {
+    console.log(error);
+  }
 };

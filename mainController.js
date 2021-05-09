8fbf6ac865a81342171b3b37e9d7defac4587c80
Wale Timothy <awtim01@gmail.com>
2021-05-09 17:18:00 +0100

    returning proper response

--- mainController.js
+++ mainController.js
@@ -5,17 +5,20 @@ const config = require('./config');
 exports.readFiles = async (req, res) => {
   const wireguardPath = `algo/configs/${config.ip_address}/wireguard`;
   const directoryPath = path.join(__dirname, wireguardPath);
+  const fileArray = [];
   try {
     fs.readdir(directoryPath, (err, files) => {
       if (err) {
-        return console.log('Unable to scan directory: ' + err);
+        return res.status(400).send({ message: 'Error', 'error': 'Unable to scan directory: ' + err });
       }
   
       files.forEach( (file) => {
+        fileArray.push(file);
         console.log(file);
       });
+      return res.status(200).send({ message: 'Succsess', 'data': fileArray });
     });
   } catch (error) {
-    console.log(error);
+    return res.status(400).send({ message: 'Error', error });
   }
 };

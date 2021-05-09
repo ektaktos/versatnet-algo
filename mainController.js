9754a74d15c909dd0fe1cb812bc023cb102c5ad1
Wale Timothy <awtim01@gmail.com>
2021-05-09 17:37:28 +0100

    changed path

--- mainController.js
+++ mainController.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const config = require('./config');
 
 exports.readFiles = async (req, res) => {
-  const wireguardPath = `/algo/configs/${config.ip_address}/wireguard`;
+  const wireguardPath = `./../algo/configs/${config.ip_address}/wireguard`;
   const directoryPath = path.join(__dirname, wireguardPath);
   const fileArray = [];
   try {

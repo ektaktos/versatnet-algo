e030df2a51483ff70f046a697e480e229ca6ec1f
Wale Timothy <awtim01@gmail.com>
2021-05-13 21:27:49 +0100

    Added function to upload file with full path

--- mainController.js
+++ mainController.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-const imageToBase64 = require('image-to-base64');
 var cloudinary = require('cloudinary').v2;
 const config = require('./config');
 const models = require('./models/index');
@@ -16,20 +15,17 @@ cloudinary.config({
 exports.readFiles = async (req, res) => {
   const wireguardPath = `./../algo/configs/${config.ip_address}/wireguard`;
   const directoryPath = path.join(__dirname, wireguardPath);
-  const fileArray = [];
-  const errors = [];
   try {
     fs.readdir(directoryPath, async (err, files) => {
       if (err) {
         return res.status(400).send({ message: 'Error', path: wireguardPath, error: 'Unable to scan directory: ' + err });
       }
       for (const file of files) {
-        const upload = await cloudinary.uploader.upload(file);
+        const fullPath = `${directoryPath}/${file}`;
+        const result = await cloudinary.uploader.upload(fullPath);
         await Image.create({ image_url: result.secure_url });
-        console.log(upload);
-        fileArray.push(upload);
       }
-      return res.status(200).send({ message: 'Success', files });
+      return res.status(200).send({ message: 'Success'});
     });
   } catch (error) {
     return res.status(400).send({ message: 'Error', error });

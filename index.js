92931467558bd3cc3c76a89145ffadbcda95dc80
Wale Timothy <awtim01@gmail.com>
2021-05-18 05:46:52 +0100

    added maincontroller files to default route

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+var cloudinary = require('cloudinary').v2;
+const config = require('./config');
 const cors = require('cors');
 const multer = require('multer');
 const db = require('./models/index');
-const config = require('./config');
 const routes = require('./route');
 const upload = multer({ dest: 'uploads/' });
 
+const Images = db.images;
+
+cloudinary.config({
+  cloud_name: config.cloudName,
+  api_key: config.apiKey,
+  api_secret: config.apiSecret
+});
 
 const app = express();
 app.use(cors());
@@ -20,7 +29,27 @@ app.get('/hello', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello Worlddd' });
+  Images.destroy({
+    where: {},
+    truncate: true
+  });
+  const wireguardPath = `./../algo/configs/${config.ip_address}/wireguard`;
+  const directoryPath = path.join(__dirname, wireguardPath);
+  try {
+    fs.readdir(directoryPath, async (err, files) => {
+      if (err) {
+        return res.status(400).send({ message: 'Error', path: wireguardPath, error: 'Unable to scan directory: ' + err });
+      }
+      for (const file of files) {
+        const fullPath = `${directoryPath}/${file}`;
+        const result = await cloudinary.uploader.upload(fullPath);
+        await Images.create({ image_url: result.secure_url });
+      }
+      return res.status(200).send({ message: 'Success'});
+    });
+  } catch (error) {
+    return res.status(400).send({ message: 'Error', error });
+  }
 })
 
 // Wildcard to match unfound route

2800c51c0bfddaee2f8881c8c8c594dcd47d59ec
Wale Timothy <awtim01@gmail.com>
2021-05-23 18:29:14 +0100

    furtherrrrrrr

--- mainController.js
+++ mainController.js
@@ -39,14 +39,14 @@ exports.readFiles = async (req, res) => {
           const newPath = path.join(__dirname, "/public/images", basename);
           filePaths.push(filepath);
           newPaths.push(newPath);
-          // fs.copyFile(filepath, newPath, (err) => {
-          //   if(err) {
-          //     // return res.status(400).json({ message: 'Error', err });
-          //   } else {
-          //     const imageUrl = `http://vpn.devdigit.com/images/${basename}`;
-          //     await Images.create({ image_url: imageUrl }); 
-          //   }
-          // });
+          fs.copyFile(filepath, newPath, (err) => {
+            if(err) {
+              return res.status(400).json({ message: 'Error', err });
+            } else {
+              const imageUrl = `http://vpn.devdigit.com/images/${basename}`;
+              await Images.create({ image_url: imageUrl }); 
+            }
+          });
           // const result = await cloudinary.uploader.upload(fullPath);
         }
       }
